a2092b634e08234455433665fb45a20fb55cf0fe
Inyang Andino <doninyangm@gmail.com>
2020-01-23 10:35:54 +0100

    added withdraw/getbalance function

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
+
 function Account(accountName, openingBalane){
     this.accountName = accountName;
     this.accountBalance = openingBalane;
+
     this.deposit = function(amount){
         this.accountBalance += amount;
         return this.accountBalance;
     }
     this.withdraw = function(amount){
         this.accountBalance -= amount
+        return this.accountBalance;
+    }
+
+    this.getBalance = function(){
         return this.accountBalance;
     }
 }
